8a5318794f7a9a322f0a2ba5cdf124648cad7deb
dhrubajyoti2012 <dhrubajyoti2012@gmail.com>
2026-05-26 02:09:31 +0530

    Update TodoApp.jsx

--- frontend/src/Components/TodoApp.jsx
+++ frontend/src/Components/TodoApp.jsx
@@ -3,8 +3,8 @@ import axios from 'axios'
 import './TodoApp.css'
 
 // 🔴 USING FULL URL - NO PROXY NEEDED!
-const API_URL = import.meta.env.VITE_API_URL; 
-//const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/todos'
+//const API_URL = import.meta.env.VITE_API_URL; 
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/todos';
 //const API_URL = 'https://mern-todo-app2-backend.onrender.com/api/todos';
 
 
